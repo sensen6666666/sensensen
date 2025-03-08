9a477d9ee81684f8e892e85c36c165d31478240b
sensen6666666 <2457308578@qq.com>
2025-03-08 20:16:49 +0800

    例题5：强制类型转换 保留四位小数

---
+++ thefirstw/src/com/itheima/demo1/CastDemo.java
@@ -0,0 +1,12 @@
+package com.itheima.demo1;
+
+public class CastDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        double pi = Math.PI;//要达到保留四位小数的效果
+        pi = pi *10000 +0.5;//四舍五入
+        pi = (int )pi;//强制转换成整数
+        pi = pi /10000;//再除以一万，达到四舍五入的效果
+        System.out.println(pi);
+    }
+}
