f83502e82faf4e498e818219e1f2922bfd5b84cf
sensen6666666 <2457308578@qq.com>
2025-03-12 20:06:52 +0800

    作业2：BMI的计算

---
+++ zuoye/src/zuoye1/dierzhang6.java
@@ -0,0 +1,16 @@
+package zuoye1;
+import java.util.Scanner;
+public class dierzhang6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float w;
+        float h;
+        float BMI;
+        System.out.print("请输入您的体重（kg）：");
+        w = input.nextFloat();
+        System.out.print("请输入您的身高（m）：");
+        h = input.nextFloat();
+        BMI = w / h / h;
+        System.out.print("您的BMI为" + BMI);
+    }
+}
