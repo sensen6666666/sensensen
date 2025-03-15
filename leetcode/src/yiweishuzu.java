845ab61515827501218913685b56874a03d86c4c
sensen6666666 <2457308578@qq.com>
2025-03-15 22:08:09 +0800

    一维数组的动态和

--- leetcode/src/yiweishuzu.java
+++ leetcode/src/yiweishuzu.java
@@ -2,7 +2,7 @@ public class yiweishuzu {
     public static void main(String[] args) {
         class Solution {
             public int[] runningSum(int[] nums) {
-                for (int i = 1; i < nums.length; i++) {
+                for (int i = 1; i < nums.length; i++) {//
                     nums[i] += nums[i - 1];//
                 }
                 return nums;//
